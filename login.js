dd068173fd84d428b0574728a9a890c0118a0d7f
FomaKiniaev000 <102984382+FomaKiniaev000@users.noreply.github.com>
2025-04-22 22:55:47 +0300

    Add login.js 1745351749167

---
+++ login.js
@@ -0,0 +1,9 @@
+export const users = [
+  { username: "student", password: "i1b8c5t7g0fa531rt8" },
+  { username: "guest", password: "a2b3c4d5e6f7g8h9i0" }
+];
+
+export const admins = [
+  { adminname: "admin", password: "z9y8x7w6v5u4t3s2r1", adminCode: "adm" },
+  { adminname: "root", password: "z9y8x7w6v5u4t3s2r1", adminCode: "adm" }
+];
